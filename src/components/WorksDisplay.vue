321c3aaa4654cb73a0506644b050d4c6e1bbbe79
承諾 <klnaserzy@gmail.com>
2024-09-21 07:55:08 +0800

    修改WorkArea的程式連結 改為modal顯示三個路徑 HomeView元素修改 WorkDisplay新增註解

--- src/components/WorksDisplay.vue
+++ src/components/WorksDisplay.vue
@@ -3,36 +3,47 @@
     import { useRouter, useRoute } from 'vue-router';
     import { works } from '@/utils/image';
 
-    const worksLen = works.length;
     const route = useRoute();
     const router = useRouter();
-    const id = computed(() => parseInt(route.params.id));
-    const workData = ref(works.find(data => data.id === id.value));
+    const id = computed(() => parseInt(route.params.id)); // 取得當前的路由 ID
+    const worksLen = works.length - 1; // 記得長度是 index 的最大值
+    const workData = ref(works.find(data => data.id === id.value)); // 根據 ID 設置初始工作數據
+    
+    // 用於向左或向右切換的 DOM 元素
     const workSwitchLeft = ref(null);
     const workSwitchRight = ref(null);
+
+    // 切換作品
     const changeWork = (move) => {
         const nextId = id.value + move;
-        if(nextId < 0 || nextId > worksLen) return 
+        if (nextId < 0 || nextId > worksLen) return; // 檢查範圍
 
-        workData.value = works.find(data => data.id === nextId);
-        router.push(`/worksArea/${id.value + move}`);
-        
-    }
-    const lowerCaseTransform = computed(() => workData.value.name.replace(/[A-Z]/g, ($1) => ' ' + $1.toLowerCase()))
+        router.push(`/worksArea/${nextId}`); // 更新 URL 路由
+    };
+    
+    // 轉換為小寫並添加空格
+    const lowerCaseTransform = computed(() => 
+        workData.value.name.replace(/[A-Z]/g, ($1) => ' ' + $1.toLowerCase())
+    );
+    
+    // 描述文本的換行處理
     const newLineText = computed(() => workData.value.describe);
-    watch(() => id.value, () => {
-        if(id.value === 0)
-            workSwitchLeft.value.style.color = '#0000002f';
-        else
-            workSwitchLeft.value.style.color = 'black';
-        
-        if(id.value === worksLen)
-            workSwitchRight.value.style.color = '#0000002f';
-        else
-            workSwitchRight.value.style.color = 'black';
 
+    // 發送 modal 開關事件
+    const emit = defineEmits(['toggleModal']);
+    const openModal = () => {
+        emit('toggleModal', workData.value.programUrl); // 傳送程式URL
+    };
+
+    // 監聽 ID 的變化
+    watch(id, () => {
+        // 更新左右箭頭的顏色
+        workSwitchLeft.value.style.color = id.value === 0 ? '#0000002f' : 'black';
+        workSwitchRight.value.style.color = id.value === worksLen ? '#0000002f' : 'black';
+
+        // 更新當前工作數據
         workData.value = works.find(data => data.id === id.value);
-    })
+    });
 </script>
 
 <template>
@@ -45,9 +56,9 @@
                     <h1>{{ lowerCaseTransform }}</h1>
                     <p v-html='newLineText'></p>
                 </article>
-                <div class='program-website-link'>
-                    <a :href='workData.programUrl' target="_blank">程式</a>
-                    <a :href='workData.webpageUrl' target="_blank">網頁畫面</a>
+                <div class='program-website-links'>
+                    <div class="link" @click='openModal()'>程式</div>
+                    <a class="link" :href='workData.webpageUrl' target="_blank">網頁畫面</a>
                 </div>
             </div>
         </main>
@@ -117,26 +128,28 @@
         white-space: nowrap;
     }
 
-    .program-website-link {
+    .program-website-links {
         display: block;
         text-align: right;
     }
     
-    .program-website-link a {
+    .program-website-links .link {
+        display: inline;
         padding: 0 10px;
         position: relative;
         transition: .1s ease-in-out;
     }
 
-    .program-website-link a:last-child {
+    .program-website-links .link:last-child {
         border-left: 1px solid rgba(0, 0, 0, 0.267);
     }
 
-    .program-website-link a:hover {
+    .program-website-links .link:hover {
         color: white;
+        cursor: pointer;
     }
 
-    .program-website-link a::after {
+    .program-website-links .link::after {
         content: '';
         top: 0;
         left: 0;
@@ -150,7 +163,7 @@
         transition: opacity .1s ease-in-out;
     }
 
-    .program-website-link a:hover::after {
+    .program-website-links .link:hover::after {
         opacity: 1;
     }
 </style>
--- src/views/WorksArea.vue
+++ src/views/WorksArea.vue
@@ -1,30 +1,136 @@
 <script setup>
+    import { ref } from 'vue';
     import { useRoute, useRouter, RouterLink } from 'vue-router';
     import WorksDisplay from '@/components/WorksDisplay.vue';
     import ChapterSelect from '@/components/ChapterSelect.vue';
-    import { ref } from 'vue';
     
     const route = useRoute();
     const router = useRouter();
+    const toggle = ref(false);
+    const githubUrl = ref('');
+    const modalCover = ref(null);
+    const modalContainer = ref(null);
     const id = ref(route.params.id);
     const changeIndex = (index) => {
         router.push(`/worksArea/${index}`);
         id.value = index;
     }
+    const onModalToggle = (programUrl) => {
+        toggle.value = true;
+        githubUrl.value = programUrl;
+    }
+
+    window.onclick = (event) => {
+        if(event.target === modalCover.value || event.target === modalContainer.value)
+            toggle.value = false;
+    }
+
 </script>
 
 <template>
-    <div class="container">
-        <RouterLink class="backHome" to="/">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+    <div class='container'>
+        <div v-show='toggle' class='modal-cover' ref='modalCover'>
+            <div class="modal-container" ref='modalContainer'>
+                <RouterLink class="modal-wrapper" :to='`${id}/program`'>
+                    <div class='modal-left'>
+                        <div class='guide'>
+                            <p>程式展示區傳送卡</p>
+                            <strong>位於當前網域內</strong>
+                        </div>
+                        <p>請選擇你要前往的卡片</p>
+                    </div>
+                    <div class="backCard">想想</div>
+                </RouterLink>
+                <div class="modal-wrapper" @click='toggle = !toggle'>
+                    <div class='modal-center'>
+                        <div class='guide'>
+                            <p>再看一次這是去哪後悔卡</p>
+                            <strong>返回</strong>
+                        </div>
+                        <p>請選擇你要前往的卡片</p>
+                    </div>
+                    <div class="backCard">你要</div>
+                </div>
+                <a class="modal-wrapper" @click='toggle = !toggle' :href='githubUrl' target='_blank'>
+                    <div class='modal-right'>
+                        <div class='guide'>
+                            <p>想離開這裡了門禁卡</p>
+                            <strong>github</strong>
+                        </div>
+                        <p>請選擇你要前往的卡片</p>
+                    </div>
+                    <div class="backCard">去哪</div>
+                </a>
+            </div>
+        </div>
+        <RouterLink class='backHome' to='/'>
+            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'><path d='M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z'/></svg>
             <p>回到首頁</p>
         </RouterLink>
-        <WorksDisplay></WorksDisplay>
+        <WorksDisplay @toggleModal='(programUrl) => onModalToggle(programUrl)'></WorksDisplay>
         <ChapterSelect :id='id' @selectIndex='(index) => changeIndex(index)'></ChapterSelect>
     </div>
 </template>
 
 <style scoped>
+    .modal-cover {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background-color: rgba(0, 0, 0, 0.6);
+        z-index: 1;
+        font-size: 1.4em;
+    }
+
+    .modal-container {
+        width: 1000px;
+        height: 100%;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+    .modal-wrapper {
+        width: 25%;
+        height: 34%;
+        text-align: center;
+        position: relative;
+    }
+
+    .modal-left, .modal-center, .modal-right, .backCard {
+        width: 100%;
+        height: 100%;
+        background-color: #f2f2f2;
+        padding: 15px;
+        border-radius: 15px;
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        transition: .3s ease-in-out;
+        transform: rotateY(0deg);
+        backface-visibility: hidden;
+    }
+
+    .modal-wrapper:hover .modal-left, .modal-wrapper:hover .modal-center, .modal-wrapper:hover .modal-right {
+        cursor: pointer;
+        transform: rotateY(180deg);
+    }
+
+    .backCard {
+        cursor: pointer;
+        transform: rotateY(-180deg);
+    }
+    
+    .modal-wrapper:hover .backCard {
+        transform: rotateY(0deg);
+    }
+    
+    strong {
+        font-weight: bold;
+    }
+
     .container {
         position: relative;
         display: block;
@@ -39,7 +145,7 @@
     }
 
     .backHome::before {
-        content: "";
+        content: '';
         width: 40px;
     }
 
